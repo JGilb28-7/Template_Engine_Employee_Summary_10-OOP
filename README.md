a4b45ba6b4526b6e7b80184ea47087d60cd14dd5
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-12-03 22:18:41 -0500

    add team.html app.js tested

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Template_Engine_Employee_Summary_10-OOP
 # Unit 10 OOP Homework: Template Engine - Employee Summary
 ## Loaded the Development Folder provided in 10_OOP HW Folder
+
+#### Notes
+`` completed tests on 12.2
--- app.js
+++ app.js
@@ -10,17 +10,178 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const devTeamMembers = [];
+// Write code to use 1.inquirer to gather information about the _development _team _members,
+// and to 2.create objects for each team member (using the correct classes as blueprints!)
+    // need to create  function () for the questions using inq and prompt the questions - JSG
+    // need to create the a way to select the employee type using list...
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function dataManager() {
+    inquirer
+        .prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter Managers Name',
+        },
+        {
+            name:'email',
+            type:'input',
+            message:'Enter the Managers Email Address',
+        },
+        {
+            name:'id',
+            type:'input',
+            message:'Enter the ID Number for Manager'
+        },
+        {
+            name:'number',
+            type:'input',
+            message:'Enter the Managers Phone Number'
+        },
+    ])
+        //jsg - need to create the response simular to last homework but using the .then in act 24 mini project
+        //Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+        //jsg - this allows the response to be pushed to the devTeamMembers array
+        .then(function(response){
+            console.log(response);
+            const newManager = new Manager(
+                response.name,
+                response.email,
+                repsonse.id,
+                response.number,
+            );
+            devTeamMembers.push(newManager);
+                dataTeam();
+            });
+        }
+    dataManager();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
+        //jsg -tested here to make sure the prompt works via node app.js in terminal - complete
+    function dataTeam() {
+        inquirer
+            .prompt([
+            {
+            name: 'team',
+            type: 'list',
+            message: 'add a team member?',
+            //use choices for list
+            choices: [
+                'select a Manager',
+                'select an Engineer',
+                'select an Intern',
+                'finished'
+            ],
+        },
+    ])
+        //use a .then again and use a switch function 
+        //Resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+
+        .then(function (data) {
+            switch (data.team) {
+                case 'select a Manager':
+                dataManager();
+                break;
+                case 'select an Engineer':
+                dataEngineer();
+                break;
+                case 'select an Intern':
+                    dataIntern();
+                    break;
+                case 'team is finished':
+                    dataTeam();
+                    break;
+            }
+            
+        });
+    }
+  
+        //jsg - repeat above for the team members with the changes for unique input
+    function dataEngineer() {
+        inquirer
+            .prompt([{
+                name: 'name',
+                type: 'input',
+                message: 'Enter Engineer Name',
+            },
+            {
+                name:'email',
+                type:'input',
+                message:'Enter Engineer Email Address',
+            },
+            {
+                name:'id',
+                type:'input',
+                message:'Enter the ID Number for Manager'
+            },
+            {
+                name:'github',
+                type:'input',
+                message:'Enter the Engineers Githib ID'
+            },
+        ])
+            .then(function(response){
+                console.log(response);
+                const newEngineer = new Engineer(
+                    response.name,
+                    response.email,
+                    repsonse.id,
+                    response.github,
+                );
+                devTeamMembers.push(newEngineer);
+                buildTeam();
+                });
+            }
+    function dataIntern() {
+        inquirer
+            .prompt([{
+                name: 'name',
+                type: 'input',
+                message: 'Enter Intern Name',
+            },
+            {
+                name:'email',
+                type:'input',
+                message:'Enter Intern Email Address',
+            },
+            {
+                name:'id',
+                type:'input',
+                message:'Enter the ID Number for Intern'
+            },
+            {
+                name:'school',
+                type:'input',
+                message:'Enter the Intern school name'
+            },
+        ])
+            .then(function(response){
+                console.log(response);
+                const newIntern = new Intern(
+                    response.name,
+                    response.email,
+                    repsonse.id,
+                    response.github,
+                );
+                devTeamMembers.push(newIntern);
+                buildTeam();
+                });
+            }
+        
+    function dataTeam(){
+   fs.writeFileSync(outputPath, render(devTeamMembers), "utf8");
+   }
+ 
+
+// After the user has input all employees desired, call the 3.`render` function (required
+// above) and 4. pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+// After you have your html, you're now ready to 5.create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named 6.`team.html` in the
+// `output` folder.
+      
+
+//You can use the variable 7.`outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
